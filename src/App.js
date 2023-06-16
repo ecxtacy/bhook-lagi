1181039d7151a60198cc188410b6fdf38c8f30c6
Ayushh404 <ayush404anand@gmail.com>
2023-06-16 10:00:38 +0530

    Made the file structure and Navbar

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Gallary from './components/pages/Gallary';
+import Login from './components/Login/Login';
+import Cart from './components/cart/Cart';
+
+function App() {
+  return (
+    <>
+    <Router>
+    <NavBar />
+      <Routes> 
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/gallary" element={<Gallary />} />
+        <Route path="/login" element={<Login />} /> 
+        <Route path="/cart" element={<Cart />} /> 
+       </Routes> 
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/Home.css';
+
+const Home = () => {
+  return (
+    <>
+    <div className='main-container'>
+
+    </div>
+    </>
+  )
+}
+
+export default Home;
